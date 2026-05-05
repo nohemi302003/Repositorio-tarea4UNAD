96c9d77a0d94d8ddeba4dc9cdfe3d0bce7cf104a
karoljulianaortiz70-dotcom <karoljulianaortiz70@gmail.com>
2026-05-04 20:25:46 -0500

    primer commit de programación

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Repositorio-tarea4UNAD
 tarea colaborativa4 PROGRAMACIÓN
+# Código de prueba para el repositorio de la UNAD
+
---
+++ codigoo.py
@@ -0,0 +1,7 @@
+# Código de prueba para el repositorio de la UNAD
+nombre = "Karol Malo"
+curso = "Ingeniería de Sistemas"
+
+print(f"Hola, mi nombre es {nombre}.")
+print(f"Estoy probando Git y GitHub desde el curso de {curso}.")
+print("¡Conexión exitosa!")
